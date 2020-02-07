961c058acbc051cd20249647f036022432bed5ea
shashbhat <shashank.kakunje@gmail.com>
2020-02-07 11:16:04 +0530

    popup

--- src/app/analytics/statement2/statement2.component.ts
+++ src/app/analytics/statement2/statement2.component.ts
@@ -2,10 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { AnalyticsService } from '../analytics.service';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 import { AuthService } from 'src/app/auth/auth.service';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { ChartSelectEvent } from 'ng2-google-charts';
-
-
 
 @Component({
   selector: 'app-statement2',
@@ -15,32 +13,32 @@ import { ChartSelectEvent } from 'ng2-google-charts';
 export class Statement2Component implements OnInit {
 
   public columnChart: GoogleChartInterface;
-  
-  animal: string;
-  name: string;
-  
-  academicYear:string[] = [];
-  term:string[] = [];
-  usn:any;
-  email:any;
-  user:string[]=[];
-  facultyName:string[] = [];
-  UE;
-  title;
-  course;
-  courseAttendance:String[] = []
-  present;
-  total;
 
-  charts : boolean = false;
-
-
-  selectedYear;
-  selectedTerm;
-
-  termDetails=[]
+  // Student vars
+  academicYear: string[] = [];
+  term: string[] = [];
+  usn: any;
+  email: any;
+  user: string[] = [];
   studentAttendanceDetails = []
   studentUEmarks = []
+  termDetails = []
+
+  // Faculty vars
+  facultyName: string[] = [];
+
+  // Chart vars
+  UE;
+  chartTitle;
+  course;
+  courseAttendance: String[] = []
+  present;
+  total;
+  charts: boolean = false;
+
+  showSpinner = false;
+  selectedYear;
+  selectedTerm;
 
   constructor(private analyticsService: AnalyticsService, private authService: AuthService, public dialog: MatDialog) { }
 
@@ -50,152 +48,143 @@ export class Statement2Component implements OnInit {
     this.get_academic_year()
     this.get_term_details()
     this.get_usn_by_email()
-  
+
   }
 
-  get_student_details(){
-  
-    if(!this.charts){
-      setTimeout(()=>{
+  get_student_details() {
+    this.showSpinner = true;
+    if (!this.charts) {
+      setTimeout(() => {
         this.showColumnChart()
-      }, 5000)
+      }, 1000)
     }
-    this.get_student_attendance_details(this.usn[0]["usn"],this.selectedTerm,this.selectedYear)
-    this.get_student_UEmarks_details(this.usn[0]["usn"],this.selectedTerm,this.selectedYear)
+    this.get_student_attendance_details(this.usn[0]["usn"], this.selectedTerm, this.selectedYear)
+    this.get_student_UEmarks_details(this.usn[0]["usn"], this.selectedTerm, this.selectedYear)
 
   }
 
-  get_usn_by_email(){
-  
-    this.analyticsService.get_usn_by_email(this.user['user']).subscribe(res=>{
-      this.usn=res['res']
+  get_usn_by_email() {
+
+    this.analyticsService.get_usn_by_email(this.user['user']).subscribe(res => {
+      this.usn = res['res']
+
       console.log(this.usn[0]["usn"])
     })
   }
 
 
-  get_academic_year(){
-    this.analyticsService.get_academic_year().subscribe(res=>{
+  get_academic_year() {
+    this.analyticsService.get_academic_year().subscribe(res => {
       this.academicYear = res["res"]
     })
   }
 
-  get_term_details(){
-    this.analyticsService.get_term_details().subscribe(res=>{
+  get_term_details() {
+    this.analyticsService.get_term_details().subscribe(res => {
       this.termDetails = res["res"]
-      
+
     })
   }
 
-  get_student_attendance_details(usn, term, academicYear){
-    this.analyticsService.get_student_attendance_details(usn, term, academicYear).subscribe(res=>{
+  get_student_attendance_details(usn, term, academicYear) {
+    this.analyticsService.get_student_attendance_details(usn, term, academicYear).subscribe(res => {
       this.studentAttendanceDetails = res["res"]
-  
+
     })
   }
 
-  get_student_UEmarks_details(usn, term, academicYear){
-    this.analyticsService.get_student_UEmarks_details(usn, term, academicYear).subscribe(res=>{
+  get_student_UEmarks_details(usn, term, academicYear) {
+    this.analyticsService.get_student_UEmarks_details(usn, term, academicYear).subscribe(res => {
       this.studentUEmarks = res["res"]
     })
   }
 
+  get_student_total_attendance() {
+    this.analyticsService.getCourseAttendance(this.course, this.usn[0]['usn']).subscribe(res => {
+      this.courseAttendance = res["res"]
+      console.log(res)
+    })
+  }
 
-  showColumnChart(){
+
+  showColumnChart() {
+    this.showSpinner = false;
     let data = []
     this.charts = true;
-    data.push(["CourseName","Attendance","Marks"]);
-    
-    setTimeout( ()=>{ 
-      for(let s of this.studentAttendanceDetails ){
-       
-        data.push([s["courseName"],s['perc']])
-    }
-    let i =1
-    for(let s of this.studentUEmarks){
+    data.push(["CourseName", "Attendance", "Marks"]);
+
+    setTimeout(() => {
+      for (let s of this.studentAttendanceDetails) {
+
+        data.push([s["courseName"], s['perc']])
+      }
+      let i = 1
+      for (let s of this.studentUEmarks) {
 
         data[i][2] = s['perc']
         i++;
-      } 
-      
-    this.showStudentDetailsChart(data)
-    console.log(data)
+      }
 
-    }, 5000 )
+      this.showStudentDetailsChart(data)
+
+
+    }, 1000)
   }
 
-  onChartSelect(event:ChartSelectEvent){
+  onChartSelect(event: ChartSelectEvent) {
     this.UE = event.selectedRowFormattedValues[2]
-   this.course = event.selectedRowFormattedValues[0]
-   console.log(this.UE)
-   console.log(this.course)
-   this.analyticsService.getCourseAttendance(this.course,this.usn).subscribe(res=>{
-     this.courseAttendance = res["res"]
-   })
-   this.present = this.courseAttendance["present"]
-   this.total = this.courseAttendance["total"]
- }
- showStudentDetailsChart(data){
-  this.title = 'Course-wise Attendance %',
-  this.columnChart={
-    chartType:"ColumnChart",
-    dataTable:data,
-    options: {
-      bar: { groupWidth: "30%" },
-      vAxis: {
-        title: "Percentage",
-      },
+    this.course = event.selectedRowFormattedValues[0]
 
-      height: 800,
-     
-      chartArea: {
-        left: 80,
-        right: 100,
-        top: 100,
-      },
-      legend: {
-        position: "top",
-        alignment: "end"
-      },
-      seriesType: "bars",
-      colors: ["#c8001d", "#0000cf"],
-      fontName: "Times New Roman",
-      fontSize: 13,
+    this.get_student_total_attendance()
 
-    }
-
-
-// showStudentDetailsChart(data){
-  
-//   this.columnChart = {
-//     chartType: 'ColumnChart',
-//     dataTable: data,
-//     options:{
-//       'width': 1300,
-//       'height': 1000,
-//       hAxis:{
-//         textStyle:{
-//           fontSize:10
-//         }
-//       }
-//     }
-//   };
-// }
-  // get_faculty_attendance_details(facultyName, term, academicYear){
-  //   this.analyticsService.get_student_attendance_details(facultyName, term, academicYear).subscribe(res=>{
-  //     this.facultyAttendanceDetails = res["res"]
-  //   }
-  
-  
-
- 
-  // onChartSelect(event){
-  //   console.log('selected')
-  //   }
-
-
-
+    setTimeout(() => {
+      console.log(this.courseAttendance)
+      this.present = this.courseAttendance["present"]
+      this.total = this.courseAttendance["total"]
+    }, 1000)
 
   }
-}
+
+
+
+  showStudentDetailsChart(data) {
+    this.chartTitle = 'Course-wise Attendance %',
+      this.columnChart = {
+        chartType: "ColumnChart",
+        dataTable: data,
+        options: {
+          bar: { groupWidth: "30%" },
+          vAxis: {
+            title: "Percentage",
+          },
+
+          height: 800,
+
+          chartArea: {
+            left: 80,
+            right: 100,
+            top: 100,
+          },
+          legend: {
+            position: "top",
+            alignment: "end"
+          },
+          seriesType: "bars",
+          colors: ["#c8001d", "#0000cf"],
+          fontName: "Times New Roman",
+          fontSize: 13,
+
+        }
+
+        // get_faculty_attendance_details(facultyName, term, academicYear){
+        //   this.analyticsService.get_student_attendance_details(facultyName, term, academicYear).subscribe(res=>{
+        //     this.facultyAttendanceDetails = res["res"]
+        //   }
+
+
+
+
+
+      }
+  }
 }
