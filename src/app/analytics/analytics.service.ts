0c822a54df2672f2bb6b721cf49964244a3c1e9c
shashbhat <shashank.kakunje@gmail.com>
2020-02-11 11:46:55 +0530

    faculty

--- src/app/analytics/analytics.service.ts
+++ src/app/analytics/analytics.service.ts
@@ -7,6 +7,7 @@ import {environment} from 'src/environments/environment'
   providedIn: 'root'
 })
 export class AnalyticsService {
+ 
 
   baseUrl = environment.baseUrl
 
@@ -41,18 +42,44 @@ export class AnalyticsService {
  
   }
   
-  // get_faculty_attendence_details(facultyName, term, academicYear):Observable<any>{
-  //   let url = `${this.baseUrl}studentAttendanceDetails/${facultyName}/${term}/${academicYear}`
-  //   return this.http.get<any>(url)
-  // }
+
   getCourseAttendance(course,usn):Observable<any>{
     let url = `${this.baseUrl}getCourseAttendance/${course}/${usn}`   
     return this.http.get<any>(url)
 
    }
+//faculty
 
-  get_student_avgMarks_faculty():Observable<any>{
-    let url = `${this.baseUrl}facultyMarksDetails/`
+
+  //  get_Faculties(dept):Observable<any>{
+  //   let url = `${this.baseUrl}facultylogin/${dept}`
+  //   return this.http.get<any>(url)
+  //  }
+
+ 
+
+  get_student_avgMarks_faculty(term, academicYear, eid):Observable<any>{
+    let url = `${this.baseUrl}facultyMarksDetails/${term}/${academicYear}/${eid}`
     return this.http.get<any>(url)
   }
+
+  get_student_avgAttendance_faculty(term, academicYear, eid):Observable<any>{
+    let url = `${this.baseUrl}facultyAttendenceDetails/${term}/${academicYear}/${eid}`
+    return this.http.get<any>(url)
+  }
+
+  get_eid_by_email(email):Observable<any>{
+    let url = `${this.baseUrl}facultyEid/${email}`
+    return this.http.get<any>(url)
+  }
+
+  get_dept_faculty(deptName):Observable<any>{
+    let URL = `${this.baseUrl}getDeptFaculty/${deptName}`   
+    return this.http.get(URL)
+   }
+
+   getFacultyName(deptId):Observable<any>{
+    let URL = `${this.baseUrl}getFacultyNameByDeptId/${deptId}`   
+    return this.http.get(URL)
+   }
 }
--- src/app/analytics/statement2/statement2.component.ts
+++ src/app/analytics/statement2/statement2.component.ts
@@ -3,6 +3,7 @@ import { AnalyticsService } from '../analytics.service';
 import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
 import { AuthService } from 'src/app/auth/auth.service';
 import { ChartSelectEvent } from 'ng2-google-charts';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-statement2',
@@ -24,10 +25,17 @@ export class Statement2Component implements OnInit {
   termDetails = []
 
   // Faculty vars
+  facultyNames
   facultyName: string[] = [];
   avgMarksFaculty
+  avgAttendanceDetails
   courseCode
+  roles;
   usnList: string[] = []
+  eid
+  set_role: any = "STUDENT"
+  user_info: any;
+  deptName
 
   // Chart vars
   UE;
@@ -40,25 +48,66 @@ export class Statement2Component implements OnInit {
   showSpinner = false;
   chart_visibility: boolean = false;
 
+
   // Error handling
   error_flag
   error_message
-  
+
   selectedYear;
   selectedTerm;
+  Faculties: any;
+
+
 
   constructor(private analyticsService: AnalyticsService, private authService: AuthService) { }
 
   ngOnInit() {
-
     this.user = this.authService.getUserInfo()
+    console.log(this.user)
+    this.roles = ""
+
+    console.log(this.user['roles'])
+    for (let i = 0; i < this.user['roles'].length; i++) {
+
+      if (this.user['roles'][i] == "FACULTY") {
+        this.roles = "faculty"
+        console.log('faculty')
+      }
+      else if(this.user['roles'].includes('STUDENT')){
+        this.roles = "student"
+      }
+
+      else if(this.user['roles'][2] == "HOD") {
+        this.roles = "hod"
+        let patt = new RegExp("[a-zA-z]*");
+        let res = patt.exec(this.eid);
+        this.deptName =res[0];
+        this.analyticsService.get_dept_faculty(this.deptName).subscribe(res => {
+          this.facultyNames = res['res']
+        })
+        }
+    }
+
+
     this.get_academic_year()
     this.get_term_details()
     this.get_usn_by_email()
-
-    this.get_student_avgMarks_faculty()
-
+    this.get_eid_by_email()
   }
+
+
+  get_user_role_type() {
+    if (this.roles == "student") {
+      this.get_student_details()
+    }
+    else if (this.roles == "faculty") {
+      this.get_faculty_details()
+    }
+    else if( this.roles == 'hod'){
+      this.get_hod_details()
+    }
+  }
+
 
   get_student_details() {
 
@@ -75,14 +124,38 @@ export class Statement2Component implements OnInit {
 
   }
 
-//login
+  get_faculty_details() {
+    this.error_flag = false;
+    this.chart_visibility = false;
+    this.showSpinner = true;
+ 
+    if (!this.chart_visibility) {
+      setTimeout(() => {
+        this.showFacultyColumnChart()
+      }, 2000)
+    }
+    this.get_student_avgAttendance_faculty(this.selectedTerm, this.selectedYear, this.eid)
+    this.get_student_avgMarks_faculty(this.selectedTerm, this.selectedYear, this.eid)
+  }
+
+
+  get_hod_details(){
+    
+  }
+
+  //login
 
   get_usn_by_email() {
 
     this.analyticsService.get_usn_by_email(this.user['user']).subscribe(res => {
       this.usn = res['res']
+    })
+  }
 
-      console.log(this.usn[0]["usn"])
+  get_eid_by_email() {
+    this.analyticsService.get_eid_by_email(this.user['user']).subscribe(res => {
+      this.eid = res['res'][0]['employeeGivenId']
+      
     })
   }
 
@@ -99,6 +172,8 @@ export class Statement2Component implements OnInit {
 
     })
   }
+
+  
 
   //student 
 
@@ -122,35 +197,41 @@ export class Statement2Component implements OnInit {
     })
   }
 
-  // Faculty
 
-  get_student_avgMarks_faculty(){
-    this.courseCode = "15ME53"
-    this.usnList = ["4MT15ME008","4MT15ME013"]
-      this.analyticsService.get_student_avgMarks_faculty().subscribe(res =>{
-        this.avgMarksFaculty = res["res"]
-        console.log(res)
-      })
+  // Faculty
+  get_student_avgAttendance_faculty(term, academicYear, eid) {
+    this.analyticsService.get_student_avgAttendance_faculty(term, academicYear, eid).subscribe(res => {
+      this.avgMarksFaculty = res["res"]
+
+    })
   }
 
-// chart
+  get_student_avgMarks_faculty(term, academicYear, eid) {
+    this.analyticsService.get_student_avgMarks_faculty(term, academicYear, eid).subscribe(res => {
+      this.avgAttendanceDetails = res["res"]
+    })
+  }
+
+
+
+  // student chart
 
   showColumnChart() {
-    
+
     let data = []
-  
+
     data.push(["CourseName", "Attendance", "Marks"]);
 
     setTimeout(() => {
       this.showSpinner = false
-    
+
       for (let s of this.studentAttendanceDetails) {
 
         data.push([s["courseName"], s['perc']])
       }
       let i = 1
       for (let s of this.studentUEmarks) {
-   
+
         data[i][2] = s['perc']
         i++;
       }
@@ -165,9 +246,6 @@ export class Statement2Component implements OnInit {
         this.error_flag = true
         this.error_message = "Data does not exist for the entered criteria"
       }
-
-      
-
 
     }, 3000)
   }
@@ -186,6 +264,41 @@ export class Statement2Component implements OnInit {
 
   }
 
+  // Faculty Chart
+  showFacultyColumnChart(){
+    let data = []
+
+    data.push(["CourseName", "Attendance", "Marks"]);
+
+    setTimeout(() => {
+      this.showSpinner = false
+      console.log(this.avgAttendanceDetails)
+      for (let s of this.avgAttendanceDetails) {
+
+        data.push([s["course"], s['Avg']])
+      }
+      let i = 1
+      console.log(this.avgMarksFaculty)
+      for (let s of this.avgMarksFaculty) {
+
+        data[i][2] = s['avg']
+        i++;
+      }
+
+      if (data.length > 1) {
+        this.chart_visibility = true
+        this.error_flag = false
+        this.showStudentDetailsChart(data)
+      }
+      else {
+
+        this.error_flag = true
+        this.error_message = "Data does not exist for the entered criteria"
+      }
+
+
+    }, 3000)
+  }
 
   showStudentDetailsChart(data) {
     this.chartTitle = 'Course-wise Attendance %',
@@ -217,17 +330,13 @@ export class Statement2Component implements OnInit {
 
         }
 
-      } 
+      }
 
-    }
-
-
-    
   }
-     
 
 
- // get_faculty_attendance_details(facultyName, term, academicYear){
-        //   this.analyticsService.get_student_attendance_details(facultyName, term, academicYear).subscribe(res=>{
-        //     this.facultyAttendanceDetails = res["res"]
-        //   }
+
+}
+
+
+
